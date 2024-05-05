c66789d1bc01d2081c55c0a9b15af237a99dbc8e
NelsonGrajales <ngrajales2332@gmail.com>
2024-05-05 12:09:35 -0500

    quicksort

--- quicksort.js
+++ quicksort.js
@@ -1,4 +1,13 @@
 let quicksort = (arr) => {
-    let pivote = arr[0];
-
+    let pivote = arr[-1];
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] <= pivote && arr[i] > arr[i + 1]) {
+            let cambio = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = cambio;
+        }
+    }
 }
+let arr = [10, 5, 8, 4, 99, 56, 23, 1, 2]
+quicksort(arr)
+console.log(arr)
